dbe9eb047062c18f120bdd39d8d3ddec684ec0c1
DesistDaydream <373406000@qq.com>
2020-12-14 14:12:23 +0800

    cookie 实验完成

--- middleware/cookie/practice/cookie.go
+++ middleware/cookie/practice/cookie.go
@@ -13,7 +13,7 @@ import (
 
 func login(c *gin.Context) {
 	// 设置 cookie
-	c.SetCookie("admin", "123456", 30, "/", "*", false, true)
+	c.SetCookie("admin", "123456", 30, "/", "datalake.cn", false, true)
 	c.String(200, "Login Success!")
 }
 
@@ -21,26 +21,29 @@ func home(c *gin.Context) {
 	c.String(200, "欢迎回家")
 }
 
-func authCookieMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// 获取客户端是否携带 cookie
-		cookie, err := c.Cookie("admin")
-		// fmt.Println(cookie)
-		if err == nil {
-			if cookie == "123456" {
-				c.Next()
-				return
-			}
+// authCookieMiddleware 认证中间件，用于拦截到 /home 的请求，并检验 cookie 信息，成功才允许访问
+func authCookieMiddleware(c *gin.Context) {
+	// 检查客户端是否携带 cookie，如果有 cookie，则判断 cookie 的值是否满足要求
+	if cookie, err := c.Cookie("admin"); err == nil {
+		switch cookie {
+		// 如果 cookie 的值为 123456，则返回主程序继续执行 Handler
+		case "123456":
+			return
+		// 如果 cookie 的值不是 123456，则响应错误信息，且中断 Handler 的后续处理
+		default:
+			c.String(http.StatusUnauthorized, "cookie 值错误")
+			c.Abort()
 		}
-
-		// 返回错误信息
+	} else {
+		// 如果客户端请求没有携带 cookie，则响应错误信息，且中断 Handler 的后续处理
 		c.String(http.StatusUnauthorized, "未登录")
-		// 防止调用待处理的处理程序。请注意，这不会停止当前的 Handler。
-		// 假设您有一个授权中间件，用于验证当前请求是否得到授权。如果授权失败（例如：密码不匹配），请调用Abort以确保不调用该请求的其余处理程序。
-		// 用白话说就是：若验证不通过，不再调用后续的 Handler 进行处理。
+		// ！！！重要！！！
+		// 防止调用待处理的 Handler。请注意，这不会停止当前的 Handler。
+		// 假设您有一个授权中间件，用于验证当前请求是否得到授权。如果授权失败(例如：密码不匹配)，则需要调用 Abort() 以确保不调用该请求的其余处理程序。
+		// 用白话说就是：若验证失败，不再调用后续的 Handler 进行处理。否则就算验证失败，依然会返回 Handler 函数继续处理后续代码。因为这个中间件只是调用链的一部分，调用完成后，还是会回到 Handler 中继续处理后面的代码
 		c.Abort()
-		return
 	}
+
 }
 
 func main() {
@@ -48,7 +51,7 @@ func main() {
 
 	r.GET("/login", login)
 
-	r.Use(authCookieMiddleware())
+	r.Use(authCookieMiddleware)
 	{
 		r.GET("/home", home)
 	}
