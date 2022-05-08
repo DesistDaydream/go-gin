4cad13b8651769e710399bbd59641723054c6727
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-05-08 17:10:01 +0800

    Update handle_json.go

--- pkg/handler/handle_json.go
+++ pkg/handler/handle_json.go
@@ -2,10 +2,10 @@ package handler
 
 import (
 	"fmt"
+	"io"
 	"time"
 
 	"encoding/json"
-	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
@@ -47,7 +47,7 @@ func HandleJSON(c *gin.Context) {
 		// curl -XPOST http://172.19.42.248:8080/json -d '{"name":"lichenhao"}'
 		//
 		// 读取 Request 的 Body
-		RequestBody, _ := ioutil.ReadAll(c.Request.Body)
+		RequestBody, _ := io.ReadAll(c.Request.Body)
 		fmt.Printf("请求体为：%v\n", string(RequestBody))
 		// 将 Request Body 的 JSON 格式转换为 struct 类型，并将 struct 中的值替换为 JSON 中的值
 		// 注意，struct 中仅传入一个 key 的值，则 struct 中也只有一个属性的值被替代，其他属性的值保持不变
